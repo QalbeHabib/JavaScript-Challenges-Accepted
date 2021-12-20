bff13a9a1f9ab1d3240f6085c654529b00d0bb11
Qalb E Habib <rana.habib313@gmail.com>
2021-12-20 13:29:59 +0500

    2nd challenge completed

--- script.js
+++ script.js
@@ -1,38 +1,42 @@
 function checkageindays() {
-    let name = prompt("Please Enter Ypur Name:")
-    let birth_year =   prompt('Enter Your Birth Year. :)')
+  let name = prompt("Please Enter Ypur Name:");
+  let birth_year = prompt("Enter Your Birth Year. :)");
 
-   if(name == '' || birth_year == ''){
-       document.getElementById('result').innerHTML = "Please Enter Your Name and DOB's Year"
-   }else{
-    let ageindays = ( 2021 - birth_year) * 365
+  if (name == "" || birth_year == "") {
+    document.getElementById("result").innerHTML =
+      "Please Enter Your Name and DOB's Year";
+  } else {
+    let ageindays = (2021 - birth_year) * 365;
 
+    let h1 = document.createElement("h1");
 
-    let h1 = document.createElement('h1')
+    h1.setAttribute("id", "result_in_days");
 
-    h1.setAttribute('id', 'result_in_days')
+    let Text_wirte = document.createTextNode(
+      name + " is " + ageindays + " Days Old"
+    );
 
-    let Text_wirte = document.createTextNode(name +' is ' + ageindays + " Days Old" )
+    h1.appendChild(Text_wirte);
 
-    h1.appendChild(Text_wirte)
-    
-    document.getElementById('result').appendChild(h1)
-   }
+    document.getElementById("result").appendChild(h1);
+  }
 }
 
 function reset_all() {
-    document.getElementById('result_in_days').remove()
+  document.getElementById("result_in_days").remove();
 }
 
+// Challange No 2 Coding start from here
+// Challange No 2 Coding start from here
+// Challange No 2 Coding start from here
 
+function add_image() {
+  let addimages = document.createElement("img");
+  addimages.src = "./download.png";
 
-function add_image(){
-   let  addimages = document.createElement('img')
-   addimages.src = './download.png'
-
-   document.getElementById('res2').appendChild(addimages)
+  document.getElementById("res2").appendChild(addimages);
+  addimages.setAttribute("id", "imgs"); // This is for removing images in the remove function (at line 42)
 }
-
-function remove_image(){
-  document.getElementsByTagName('img').remove()
+function remove_image() {
+  document.getElementById("imgs").remove();
 }
